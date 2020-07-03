3981e7ee242b53ac5b95498f5512a28277ae2cdf
Will Russell <will@wrussell.co.uk>
2020-07-03 10:01:39 +0100

    Remove background loop

--- src/bot.py
+++ src/bot.py
@@ -14,7 +14,6 @@ bot = commands.Bot(command_prefix='-xkcd ')
 
 def main():
     load_dotenv()
-    bot.loop.create_task(check_for_img())
     bot.run(os.getenv('TOKEN'))
 
 def get_img(url):
@@ -26,15 +25,6 @@ def get_img(url):
 
 def get_url():
     return URL_START + str(randrange(1,2289)) + URL_END
-
-async def check_for_img():
-    await bot.wait_until_ready()
-    xkcd_channel = bot.get_channel(int(os.getenv('CHANNEL_ID')))
-
-    while True:
-        img = get_img(get_url())
-        await xkcd_channel.send(file=discord.File(img, 'daily_comic.png'))
-        await asyncio.sleep(3600)
 
 @bot.command(description='Get a random comic')
 async def comic(ctx):
