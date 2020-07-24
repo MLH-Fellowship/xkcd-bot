cc03851423dad73c71e13a5d9633b7a94ce07745
Will Russell <will@wrussell.co.uk>
2020-07-24 15:55:14 +0100

    Automatically get latest comic

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import sys
 import asyncio
 from discord.ext import commands
 import requests
@@ -9,12 +10,13 @@ from random import randrange
 
 URL_START = "https://xkcd.com/"
 URL_END = "/info.0.json"
-TOTAL_COMICS = 2336
+LATEST_COMIC = "https://xkcd.com/info.0.json"
 
 bot = commands.Bot(command_prefix='-xkcd ')
 
 def main():
     load_dotenv()
+    sys.stdout.flush()
     bot.run(os.getenv('TOKEN'))
 
 @bot.event
@@ -24,6 +26,12 @@ async def on_ready():
                                       type=discord.ActivityType.watching)
     await bot.change_presence(status=discord.Status.online, activity=activity)
 
+def get_total_comics():
+    print('Getting latest comic..')
+    response = requests.get(LATEST_COMIC).json()
+    print(f"Total: {response['num']}")
+    return response['num']
+
 def get_img(url):
     print('Getting comic...')
     response = requests.get(url).json()
@@ -31,12 +39,13 @@ def get_img(url):
     img = BytesIO(requests.get(response["img"]).content)
     return img
 
-def get_url():
-    return URL_START + str(randrange(1,TOTAL_COMICS)) + URL_END
+def get_url(total_comics):
+    return URL_START + str(randrange(1, total_comics)) + URL_END
 
 @bot.command(description='Get a random comic')
 async def comic(ctx):
-    img = get_img(get_url())
+    total = get_total_comics()
+    img = get_img(get_url(total))
     await ctx.send(file=discord.File(img, 'comamnd_comic.png'))
 
 if __name__ == '__main__':
